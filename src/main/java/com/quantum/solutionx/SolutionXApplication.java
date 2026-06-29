aee7d0cca1fadc553ccc5ad8ffdd3f3e02aa5669
量子罐 <690499491@qq.com>
2026-06-29 21:28:50 +0800

    first commit

---
+++ src/main/java/com/quantum/solutionx/SolutionXApplication.java
@@ -0,0 +1,13 @@
+package com.quantum.solutionx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quantum/solutionx/SolutionXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum.solutionx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
